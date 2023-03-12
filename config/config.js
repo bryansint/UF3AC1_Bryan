fd68163ae934ad5faad8a33d9426acb630a0ca46
bryansint <bryansanchezm1k@gmail.com>
2023-03-12 15:15:43 +0100

    tarea7

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://newUser:12345@brestaencasa.k7umsnb.mongodb.net/?retryWrites=true&w=majority"
+  // mongoURI: "mongodb+srv://newUser:12345@brestaencasa.k7umsnb.mongodb.net/?retryWrites=true&w=majority"
+  mongoURI: process.env.MONGOURI
 };
